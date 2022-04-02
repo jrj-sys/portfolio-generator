a564f2e3505850eec60e8a9cc6ec5be78fb80810
jrj-sys <jamesjamel99@gmail.com>
2022-04-02 01:44:15 -0500

    node generate HTML page

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    
+    console.log('Portfolio complete! Check out index.html to see the output.');
+});
+
 // const printProfileData = profileDataArr => {
 //     // This...
 //     for (let i = 0; i < profileDataArr.length; i++) {
@@ -17,29 +27,3 @@ const [name, github] = profileDataArgs;
 // };
 
 // printProfileData(profileDataArgs);
-
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
-
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-}
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output.');
-});
-
