5ca86425fd00f90f35ab3c973d05eee7ea7fcd36
jrj-sys <jamesjamel99@gmail.com>
2022-04-02 03:44:32 -0500

    inquirer install and functionality

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
     
-    console.log('Portfolio complete! Check out index.html to see the output.');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output.');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
 // const printProfileData = profileDataArr => {
 //     // This...
@@ -26,4 +35,3 @@ fs.writeFile('index.html', generatePage(name, github), err => {
 //     profileDataArr.forEach(profileItem => console.log(profileItem));
 // };
 
-// printProfileData(profileDataArgs);
